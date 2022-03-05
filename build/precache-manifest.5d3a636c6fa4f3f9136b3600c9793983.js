593b937483b6c3e446e746c830c521fdfbee18d6
Cale <ccarlosalejandros@gmail.com>
2022-03-05 00:39:32 -0500

    Build folder added

---
+++ build/precache-manifest.5d3a636c6fa4f3f9136b3600c9793983.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721b363ec0cc8de894528dada475a15c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92e56aa4c1d898d7d24a",
+    "url": "/static/css/main.77aaeac2.chunk.css"
+  },
+  {
+    "revision": "10a315aa361713b187cd",
+    "url": "/static/js/2.25dc4b45.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.25dc4b45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e56aa4c1d898d7d24a",
+    "url": "/static/js/main.e2d7dbae.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/static/media/ai.17ade557.png"
+  }
+]);
